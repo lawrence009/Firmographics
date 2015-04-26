233b91b0adeae223a2942b126ac0abc461b3d216
Lawrence Chen <lawrence009@outlook.com>
2015-04-26 09:33:07 -0700

    load and explore data

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 #Firmographics sample of U.S. businesses
+
+##SS_IG_Data_Appended_03192015.csv
+Client dimension from Omega database appended with Infographics data.
+The first 11 columns (_SS suffix) from Omega database.
+Columns 12 and 13 are assumed to be appended as part of matching algorithm.
+The remaining columns are appended from Infographics.
+
+
+##SS_IG_Ultimate_Data.csv
+SS_IG_Data_Appended_03192015.csv by Ultimate Parent only
+subsetted by Jeff on 3/26/2015
+
+##ULTIMATE_WORK_UP.csv
+SS_IG_Data_Appended_03192015.csv by LOCATION_NUMBER only
+subsetted by Jeff on 3/30/2015
+
--- load.R
+++ load.R
@@ -1,8 +1,33 @@
+#script to explore firmographic data
 
 
-df <- read.csv('SS_IG_Data_Appended_03192015.csv')
 
-ultimate <- read.csv('SS_IG_Ultimate_Data.csv')
-ultimate$PHONE <- as.numeric(ultimate$PHONE)
+filelist <- list.files(pattern = '.csv$', full.names = T)
 
-ultimate.workup <- read.csv('ULTIMATE_WORK_UP.csv')
+df <- read.csv(filelist[1], na.strings = '')
+
+head(df[, c('COMMON_CLIENT_ID_SS', 'LOCATION_NUMBER', 'SUBSIDIARY_NUMBER','ULTIMATE_PARENT_NUM')], 25)
+
+#ultimate and workup are Jeff's attempts to explore the infographics
+ultimate <- read.csv(filelist[2]) #1904 obs.
+workup <- read.csv(filelist[3]) #26358 obs.
+
+
+
+###
+hasLocationNum <- which(!is.na(df$LOCATION_NUMBER)) #25906 obs.
+hasUltimateNum <- which(!is.na(df$ULTIMATE_PARENT_NUM)) #9374 obs.
+hasSubsidiary <- which(!is.na(df$SUBSIDIARY_NUMBER)) #5605 obs.
+uniqueLoc <- order(unique(df$LOCATION_NUMBER[hasLocationNum]))
+
+getDups <- function(df, col = 1) {
+    # df is a data frame
+    # col is the column number used to check for duplicates
+    
+    isDup <- duplicated(df[, col])
+    dups <- df[, col] %in% df[isDup, col]
+    dups <- df[dups, ]
+    dups <- dups[order(dups[, col]), ]
+}
+
+dups <- getDups(df, 14) #col 14 is LOCATION_NUMBER
